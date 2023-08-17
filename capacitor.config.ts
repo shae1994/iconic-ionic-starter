a2f06089349485be769c793208b237c72300f346
Juddy-Shae Wallace <juddy-shae@Juddy-Shaes-MacBook-Pro.local>
2023-08-17 11:00:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Juddys-iconic-ionic-app',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
